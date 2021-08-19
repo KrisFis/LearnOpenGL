0a0ca5655a910830e5987787fb618dde1d90421a
Jan Kristián Fišera <alternity@email.cz>
2021-08-19 14:42:39 +0200

    Transition to 3D

--- LearnOpenGL/Source/Private/main.cpp
+++ LearnOpenGL/Source/Private/main.cpp
@@ -15,6 +15,11 @@
 // typedefs
 typedef unsigned int FId;
 
+// Window
+static unsigned short WindowWidth = 800;
+static unsigned short WindowHeight = 600;
+
+// Global
 static float DeltaSeconds = 0.f;
 
 // Test
@@ -24,6 +29,9 @@ static float PositionY = 0.f, PositionX = 0.f;
 
 void WindowSizeChanged(GLFWwindow* Window, int Width, int Height)
 {
+	WindowWidth = Width;
+	WindowHeight = Height;
+
 	glViewport(0, 0, Width, Height);
 }
 
@@ -85,11 +93,6 @@ void ProcessInput(GLFWwindow* Window)
 	else if (glfwGetKey(Window, GLFW_KEY_F2) == GLFW_PRESS)
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	// Opacity
-	{
-
-	}
-
 	// Rotation && Translation && Opacity
 	{
 		if(shiftPressed)
@@ -145,7 +148,7 @@ void BindEvents(GLFWwindow* Window)
 	glfwSetFramebufferSizeCallback(Window, &WindowSizeChanged);
 }
 
-void ProcessDraw(const FId& VertexArrayId, FShader ShaderToUse, FTexture TexturesToUse[2])
+void ProcessDraw(const FId& VertexArrayId, FShader ShaderToUse, FTexture TexturesToUse[2], glm::vec3 cubePositions[10])
 {
 	// Clear part
 	{
@@ -153,80 +156,120 @@ void ProcessDraw(const FId& VertexArrayId, FShader ShaderToUse, FTexture Texture
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
 		// Clears screen with ClearColor
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
 	// Defines which program to use
-	ShaderToUse.Use();
-
-	// Textures
+	// * and init
+	if(!ShaderToUse.IsUsed())
 	{
-		ShaderToUse.SetInt("texture1", 0);
-		ShaderToUse.SetInt("texture2", 1);
-		ShaderToUse.SetFloat("lerpValue", Opacity);
+		ShaderToUse.Use();
 
-		TexturesToUse[0].Use(0);
-		TexturesToUse[1].Use(1);
-	}
+		// Textures
+		{
+			TexturesToUse[0].Use(0);
+			TexturesToUse[1].Use(1);
 
-	// Trans Mat
-	{
-		glm::mat4 trans = glm::mat4(1.f);
-		trans =	glm::rotate(trans, Rotation, glm::vec3(0.0f, 0.0f, 1.0f));
-		trans = glm::translate(trans, glm::vec3(PositionY, PositionX, 0.f));
+			ShaderToUse.SetInt("texture1", 0);
+			ShaderToUse.SetInt("texture2", 1);
+		}
 
-		ShaderToUse.SetMatrix4("transform", trans);
+		// MAT4
+		{
+			glm::mat4 view = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -3.f));
+			glm::mat4 projection = glm::perspective(glm::radians(45.f), (float)WindowWidth / (float)WindowHeight, 0.1f, 100.f);
+
+			ShaderToUse.SetMatrix4("view", view);
+			ShaderToUse.SetMatrix4("projection", projection);
+		}
 	}
 
 	// Binds vertex array
 	glBindVertexArray(VertexArrayId);
 
-	// TRIANGLES
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	// Textures
+	{
+		ShaderToUse.SetFloat("lerpValue", Opacity);
+	}
+
+	// MAT4
+	for(unsigned char i = 0; i < 10; ++i)
+	{
+		glm::mat4 model = glm::mat4(1.0f);
+  		model = glm::translate(model, cubePositions[i]);
+  		model = glm::rotate(model, glm::radians(20.0f * i) + (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+		ShaderToUse.SetMatrix4("model", model);
+
+		// TRIANGLES
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
 }
 
-bool CreateAndBindSquare(const FId& VertexArrayId, FId* OutBuffersIds[2])
+bool CreateAndBindCube(const FId& VertexArrayId, FId* OutBufferId)
 {
 	float vertices[] = {
-		// positions          // colors           // texture coords
-		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+	    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
-	unsigned int indices[] = {
-		 0, 1, 3, // first triangle
-		 1, 2, 3  // second triangle
-	};
-
-	FId resultBuffersIds[2];
-	glGenBuffers(2, resultBuffersIds);
+	FId resultBufferId;
+	glGenBuffers(1, &resultBufferId);
 
 	glBindVertexArray(VertexArrayId);
 
-	glBindBuffer(GL_ARRAY_BUFFER, resultBuffersIds[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, resultBufferId);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, resultBuffersIds[1]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	// texture coord attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-	// Unbind vertex array
 	glBindVertexArray(0);
 
-	if (OutBuffersIds) *OutBuffersIds = resultBuffersIds;
+	if (OutBufferId) *OutBufferId = resultBufferId;
 	return true;
 }
 
@@ -251,7 +294,7 @@ bool CreateInitWindow(GLFWwindow** OutWindow)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* resultWindow = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	GLFWwindow* resultWindow = glfwCreateWindow(WindowWidth, WindowHeight, "LearnOpenGL", nullptr, nullptr);
 	if (!resultWindow)
 	{
 		std::cout << "Failed to create window" << std::endl;
@@ -267,7 +310,8 @@ bool CreateInitWindow(GLFWwindow** OutWindow)
 	}
 
     BindEvents(resultWindow);
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, WindowWidth, WindowHeight);
+	glEnable(GL_DEPTH_TEST);
 
 	if (OutWindow) *OutWindow = resultWindow;
 	return true;
@@ -289,7 +333,7 @@ int main()
 		return -2;
 	}
 
- 	if (!CreateAndBindSquare(vertexArrayId, nullptr /* WE DO NOT CHANGE BUFFERS NOW */))
+ 	if (!CreateAndBindCube(vertexArrayId, nullptr /* WE DO NOT CHANGE BUFFERS NOW */))
  	{
  		glfwTerminate();
  		return -3;
@@ -309,6 +353,19 @@ int main()
 		return -5;
 	}
 
+ 	glm::vec3 cubePositions[] = {
+ 	    glm::vec3( 0.0f,  0.0f,  0.0f),
+ 	    glm::vec3( 2.0f,  5.0f, -15.0f),
+ 	    glm::vec3(-1.5f, -2.2f, -2.5f),
+ 	    glm::vec3(-3.8f, -2.0f, -12.3f),
+ 	    glm::vec3( 2.4f, -0.4f, -3.5f),
+ 	    glm::vec3(-1.7f,  3.0f, -7.5f),
+ 	    glm::vec3( 1.3f, -2.0f, -2.5f),
+ 	    glm::vec3( 1.5f,  2.0f, -2.5f),
+ 	    glm::vec3( 1.5f,  0.2f, -1.5f),
+ 	    glm::vec3(-1.3f,  1.0f, -1.5f)
+ 	};
+
 	// Main render loop
 	FTimer frameTimer;
 	while (!glfwWindowShouldClose(mainWindow))
@@ -316,7 +373,7 @@ int main()
 		frameTimer.Start();
 
 		ProcessInput(mainWindow);
-		ProcessDraw(vertexArrayId, testShader, testTextures);
+		ProcessDraw(vertexArrayId, testShader, testTextures, cubePositions);
 
 		glfwSwapBuffers(mainWindow);
 		glfwPollEvents();
