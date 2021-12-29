e4ceae39f4bcbefcf26f60ddaad6fd82711c49cc
KrisFis <krisfis@email.cz>
2021-12-29 15:08:40 +0100

    Added example imgui

--- LearnOpenGL/Source/Private/App/main.cpp
+++ LearnOpenGL/Source/Private/App/main.cpp
@@ -236,6 +236,18 @@ bool CreateInitWindow(GLFWwindow*& OutWindow)
 	return true;
 }
 
+bool CreateInitUI(GLFWwindow* Window)
+{
+	ImGui::CreateContext();
+
+	ImGui_ImplGlfw_InitForOpenGL(Window, true);
+	ImGui_ImplOpenGL3_Init("#version 460");
+
+	ImGui::StyleColorsDark();
+
+	return true;
+}
+
 bool PrepareSkybox(FSceneObjectPtr& OutSkyboxObj)
 {
 	OutSkyboxObj = FSkybox::Create(
@@ -549,6 +561,11 @@ bool InitRender(TFastMap<EUniformBufferMainType, FUniformBufferPtr>& Uniforms)
 	return true;
 }
 
+bool InitUI()
+{
+	return true;
+}
+
 void ProcessRender(TFastMap<EShaderMainType, FShaderProgramPtr>& Shaders, TFastMap<EUniformBufferMainType, FUniformBufferPtr>& Uniforms)
 {
 	uint16 windowWidth, windowHeight;
@@ -653,6 +670,48 @@ void ProcessRender(TFastMap<EShaderMainType, FShaderProgramPtr>& Shaders, TFastM
 	}
 }
 
+void ProcessUIRender()
+{
+	static bool show_demo_window = false;
+	static bool show_another_window = false;
+	static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	{
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Hello, world!");
+
+		ImGui::Text("This is some useful text.");
+		ImGui::Checkbox("Demo Window", &show_demo_window);
+		ImGui::Checkbox("Another Window", &show_another_window);
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+		ImGui::ColorEdit3("clear color", (float*)&clear_color);
+
+		 if (ImGui::Button("Button"))
+			 counter++;
+
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+
+		if (show_another_window)
+		{
+			ImGui::Begin("Another Window", &show_another_window);
+			ImGui::Text("Hello from another window!");
+			if (ImGui::Button("Close Me"))
+				show_another_window = false;
+			ImGui::End();
+		}
+	}
+}
+
 bool EngineInit()
 {
 	GCamera = FCamera::Create(
@@ -691,36 +750,46 @@ int32 GuardedMain()
 		return -1;
 	}
 
+	if(!CreateInitUI(GWindow))
+	{
+		return -2;
+	}
+
 	TFastMap<EShaderMainType, FShaderProgramPtr> Shaders;
 	TFastMap<EUniformBufferMainType, FUniformBufferPtr> Uniforms;
 	if(!PrepareShaders(Shaders, Uniforms))
 	{
-		return -2;
+		return -3;
 	}
 	
 	if(!PrepareFBs(GScreenObject, GMSAAFramebuffer, GScreenFramebuffer))
 	{
-		return -3;
+		return -4;
 	}
 	
 	if(!PrepareScene(GScene))
 	{
-		return -4;
+		return -5;
 	}
 	
 	if(!PrepareSkybox(GSkyboxObject))
 	{
-		return -5;
+		return -6;
 	}
 
 	if(!EngineInit())
 	{
-		return -6;
+		return -7;
 	}
 	
 	if(!InitRender(Uniforms))
 	{
-		return -7;
+		return -8;
+	}
+
+	if(!InitUI())
+	{
+		return -9;
 	}
 
 	// Main render loop
@@ -737,11 +806,26 @@ int32 GuardedMain()
 		ProcessInput();
 		ProcessRender(Shaders, Uniforms);
 
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ProcessUIRender();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 		glfwSwapBuffers(GWindow);
 		glfwPollEvents();
 
 		frameTimer.Stop();
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwTerminate();
 
 	return 0;
 }
@@ -759,7 +843,6 @@ int32 main()
 	// Shutdown
 	{
 		NRenderUtils::Shutdown();
-		glfwTerminate();
 	}
 
 	// End print
--- LearnOpenGL/Source/Public/ModuleMinimal.h
+++ LearnOpenGL/Source/Public/ModuleMinimal.h
@@ -31,3 +31,4 @@
 // ImGUI
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
