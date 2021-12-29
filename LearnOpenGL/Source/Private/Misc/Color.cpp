cc23c6a68989b9dbc6dff98f48911dc6f634fea2
Jan Kristian Fisera <krisfis@email.cz>
2021-12-29 03:03:32 +0100

    Tweaked colors to be able to contain HDR color (overexposed light)

--- LearnOpenGL/Source/Private/Misc/Color.cpp
+++ LearnOpenGL/Source/Private/Misc/Color.cpp
@@ -4,124 +4,73 @@
 
 namespace NColorPrivate
 {
-	FORCEINLINE FColor& FillFrom(FColor& OutColor, const FColor& Other)
+	FORCEINLINE void CheckAdd(const FColor& A, const FColor& B)
 	{
-		OutColor.R = Other.R;
-		OutColor.G = Other.G;
-		OutColor.B = Other.B;
-		OutColor.A = Other.A;
-		return OutColor;
+		#if BUILD_DEBUG
+		ENSURE(A.R <= UINT16_MAX - B.R);
+		ENSURE(A.G <= UINT16_MAX - B.G);
+		ENSURE(A.B <= UINT16_MAX - B.B);
+		ENSURE(A.A <= UINT16_MAX - B.A);
+		#endif // BUILD_DEBUG
+	}
+	
+	FORCEINLINE void CheckSubstract(const FColor& A, const FColor& B)
+	{
+		#if BUILD_DEBUG
+		ENSURE(A.R < B.R);
+		ENSURE(A.G < B.G);
+		ENSURE(A.B < B.B);
+		ENSURE(A.A < B.A);
+		#endif // BUILD_DEBUG
+	}
+	
+	FORCEINLINE void CheckMultiply(const FColor& A, const FColor& B)
+	{
+		#if BUILD_DEBUG
+		ENSURE(A.R > UINT16_MAX / B.R);
+		ENSURE(A.G > UINT16_MAX / B.G);
+		ENSURE(A.B > UINT16_MAX / B.B);
+		ENSURE(A.A > UINT16_MAX / B.A);
+		#endif // BUILD_DEBUG
+	}
+	
+	FORCEINLINE void CheckMultiply(const FColor& A, float Value)
+	{
+		#if BUILD_DEBUG
+		ENSURE(A.R > UINT16_MAX / Value);
+		ENSURE(A.G > UINT16_MAX / Value);
+		ENSURE(A.B > UINT16_MAX / Value);
+		ENSURE(A.A > UINT16_MAX / Value);
+		#endif // BUILD_DEBUG
+	}
+	
+	FORCEINLINE void CheckDivide(const FColor& A, const FColor& B)
+	{
+		#if BUILD_DEBUG
+		ENSURE(B.R > 0);
+		ENSURE(B.G > 0);
+		ENSURE(B.B > 0);
+		ENSURE(B.A > 0);
+		#endif // BUILD_DEBUG
+	}
+	
+	FORCEINLINE void CheckDivide(const FColor& A, float Value)
+	{
+		#if BUILD_DEBUG
+		ENSURE(Value > 0);
+		#endif // BUILD_DEBUG
 	}
 }
 
-FColor FColor::operator+(const FColor& Other) const
+const FColor& FColor::GetEmpty()
 {
-	FColor newColor;
-	newColor.R = (R > UINT8_MAX - Other.R) ? UINT8_MAX : R + Other.R;
-	newColor.G = (G > UINT8_MAX - Other.G) ? UINT8_MAX : G + Other.G;
-	newColor.B = (B > UINT8_MAX - Other.B) ? UINT8_MAX : B + Other.B;
-	newColor.A = (A > UINT8_MAX - Other.A) ? UINT8_MAX : A + Other.A;
-	return newColor;
+	static FColor emptyVal = FColor();
+	return emptyVal;
 }
 
-FColor& FColor::operator+=(const FColor& Other)
+FColor FColor::FromCMYK(float Cyan, float Magenta, float Yellow, float Black)
 {
-	return NColorPrivate::FillFrom(*this, operator+(Other));
-}
-
-FColor FColor::operator-(const FColor& Other) const
-{
-	FColor newColor;
-	newColor.R = (R < Other.R) ? 0 : R - Other.R;
-	newColor.G = (G < Other.G) ? 0 : G - Other.G;
-	newColor.B = (B < Other.B) ? 0 : B - Other.B;
-	newColor.A = (A < Other.A) ? 0 : A - Other.A;
-	return newColor;
-}
-
-FColor& FColor::operator-=(const FColor& Other)
-{
-	return NColorPrivate::FillFrom(*this, operator-(Other));
-}
-
-FColor FColor::operator*(const FColor& Other) const
-{
-	FColor newColor;
-	newColor.R = (uint8)glm::clamp<float>((float)R * (float)Other.R / UINT8_MAX, 0.f, UINT8_MAX);
-	newColor.G = (uint8)glm::clamp<float>((float)G * (float)Other.G / UINT8_MAX, 0.f, UINT8_MAX);
-	newColor.B = (uint8)glm::clamp<float>((float)B * (float)Other.B / UINT8_MAX, 0.f, UINT8_MAX);
-	newColor.A = (uint8)glm::clamp<float>((float)A * (float)Other.A / UINT8_MAX, 0.f, UINT8_MAX);
-	return newColor;
-}
-
-FColor& FColor::operator*=(const FColor& Other)
-{
-	return NColorPrivate::FillFrom(*this, operator*(Other));
-}
-
-FColor FColor::operator/(const FColor& Other) const
-{
-	FColor newColor;
-	newColor.R = (uint8)glm::clamp<float>((float)R / (float)Other.R / UINT8_MAX, 0.f, UINT8_MAX);
-	newColor.G = (uint8)glm::clamp<float>((float)G / (float)Other.G / UINT8_MAX, 0.f, UINT8_MAX);
-	newColor.B = (uint8)glm::clamp<float>((float)B / (float)Other.B / UINT8_MAX, 0.f, UINT8_MAX);
-	newColor.A = (uint8)glm::clamp<float>((float)A / (float)Other.A / UINT8_MAX, 0.f, UINT8_MAX);
-	return newColor;
-}
-
-FColor& FColor::operator/=(const FColor& Other)
-{
-	return NColorPrivate::FillFrom(*this, operator/(Other));
-}
-
-FColor FColor::operator+(uint8 Value) const
-{
-	return operator+(FColor(Value,Value,Value,Value));
-}
-
-FColor& FColor::operator+=(uint8 Value)
-{
-	return operator+=(FColor(Value,Value,Value,Value));
-}
-
-FColor FColor::operator-(uint8 Value) const
-{
-	return operator-(FColor(Value,Value,Value,Value));
-}
-
-FColor& FColor::operator-=(uint8 Value)
-{
-	return operator-=(FColor(Value,Value,Value,Value));
-}
-
-FColor FColor::operator*(float Value) const
-{
-	FColor newColor;
-	newColor.R = (uint8)glm::clamp<float>((float)R * Value, 0.f, UINT8_MAX);
-	newColor.G = (uint8)glm::clamp<float>((float)G * Value, 0.f, UINT8_MAX);
-	newColor.B = (uint8)glm::clamp<float>((float)B * Value, 0.f, UINT8_MAX);
-	newColor.A = (uint8)glm::clamp<float>((float)A * Value, 0.f, UINT8_MAX);
-	return newColor;
-}
-
-FColor& FColor::operator*=(float Value)
-{
-	return NColorPrivate::FillFrom(*this, operator*(Value));
-}
-
-FColor FColor::operator/(float Value) const
-{
-	FColor newColor;
-	newColor.R = (uint8)glm::clamp<float>((float)R / Value, 0.f, UINT8_MAX);
-	newColor.G = (uint8)glm::clamp<float>((float)G / Value, 0.f, UINT8_MAX);
-	newColor.B = (uint8)glm::clamp<float>((float)B / Value, 0.f, UINT8_MAX);
-	newColor.A = (uint8)glm::clamp<float>((float)A / Value, 0.f, UINT8_MAX);
-	return newColor;
-}
-
-FColor& FColor::operator/=(float Value)
-{
-	return NColorPrivate::FillFrom(*this, operator/(Value));
+	return FColor((1-Cyan) * (1-Black), (1-Magenta) * (1-Black), (1-Yellow) * (1-Black), (float)UINT8_MAX);
 }
 
 FColor FColor::FromHex(const char* Value)
@@ -139,16 +88,12 @@ FColor FColor::FromHex(const char* Value)
 
 FColor FColor::FromVec4(const glm::vec4& Value)
 {
-	glm::vec4 normValue = glm::clamp(Value, 0.f, 1.f);
-	normValue *= (float)UINT8_MAX;
-	return { (uint8)normValue.x, (uint8)normValue.y, (uint8)normValue.z, (uint8)normValue.w };
+	return {(uint16)Value.x, (uint16)Value.y, (uint16)Value.z, (uint16)Value.w };
 }
 
 FColor FColor::FromVec3(const glm::vec3& Value)
 {
-	glm::vec3 normValue = glm::clamp(Value, 0.f, 1.f);
-	normValue *= (float)UINT8_MAX;
-	return { (uint8)normValue.x, (uint8)normValue.y, (uint8)normValue.z, UINT8_MAX };
+	return {(uint16)Value.x, (uint16)Value.y, (uint16)Value.z, UINT8_MAX };
 }
 
 glm::vec4 FColor::ToVec4() const
@@ -159,4 +104,178 @@ glm::vec4 FColor::ToVec4() const
 glm::vec3 FColor::ToVec3() const
 {
 	return {(float)R / UINT8_MAX, (float)G / UINT8_MAX, (float)B / UINT8_MAX };
+}
+
+void FColor::Normalize()
+{
+	R = glm::clamp<uint16>(R, 0, UINT8_MAX);
+	G = glm::clamp<uint16>(G, 0, UINT8_MAX);
+	B = glm::clamp<uint16>(B, 0, UINT8_MAX);
+	A = glm::clamp<uint16>(A, 0, UINT8_MAX);
+}
+
+void FColor::Normalize(FColor& OutColor)
+{
+	OutColor.R = glm::clamp<uint16>(R, 0, UINT8_MAX);
+	OutColor.G = glm::clamp<uint16>(G, 0, UINT8_MAX);
+	OutColor.B = glm::clamp<uint16>(B, 0, UINT8_MAX);
+	OutColor.A = glm::clamp<uint16>(A, 0, UINT8_MAX);
+}
+
+FColor FColor::operator+(const FColor& Other) const
+{
+	NColorPrivate::CheckAdd(*this, Other);
+	
+	FColor newColor;
+	newColor.R = R + Other.R;
+	newColor.G = G + Other.G;
+	newColor.B = B + Other.B;
+	newColor.A = A + Other.A;
+	return newColor;
+}
+
+FColor& FColor::operator+=(const FColor& Other)
+{
+	NColorPrivate::CheckAdd(*this, Other);
+	
+	R += Other.R;
+	G += Other.G;
+	B += Other.B;
+	A += Other.A;
+	return *this;
+}
+
+FColor FColor::operator-(const FColor& Other) const
+{
+	NColorPrivate::CheckSubstract(*this, Other);
+
+	FColor newColor;
+	newColor.R = R - Other.R;
+	newColor.G = G - Other.G;
+	newColor.B = B - Other.B;
+	newColor.A = A - Other.A;
+	return newColor;
+}
+
+FColor& FColor::operator-=(const FColor& Other)
+{
+	NColorPrivate::CheckSubstract(*this, Other);
+
+	R -= Other.R;
+	G -= Other.G;
+	B -= Other.B;
+	A -= Other.A;
+	return *this;
+}
+
+FColor FColor::operator*(const FColor& Other) const
+{
+	NColorPrivate::CheckMultiply(*this, Other);
+
+	FColor newColor;
+	newColor.R = R * Other.R;
+	newColor.G = G * Other.G;
+	newColor.B = B * Other.B;
+	newColor.A = A * Other.A;
+	return newColor;
+}
+
+FColor& FColor::operator*=(const FColor& Other)
+{
+	NColorPrivate::CheckMultiply(*this, Other);
+
+	R *= Other.R;
+	G *= Other.G;
+	B *= Other.B;
+	A *= Other.A;
+	return *this;
+}
+
+FColor FColor::operator/(const FColor& Other) const
+{
+	NColorPrivate::CheckDivide(*this, Other);
+
+	FColor newColor;
+	newColor.R = R / Other.R;
+	newColor.G = G / Other.G;
+	newColor.B = B / Other.B;
+	newColor.A = A / Other.A;
+	return newColor;
+}
+
+FColor& FColor::operator/=(const FColor& Other)
+{
+	NColorPrivate::CheckDivide(*this, Other);
+
+	R /= Other.R;
+	G /= Other.G;
+	B /= Other.B;
+	A /= Other.A;
+	return *this;
+}
+
+FColor FColor::operator+(uint16 Value) const
+{
+	return operator+(FColor(Value,Value,Value,Value));
+}
+
+FColor& FColor::operator+=(uint16 Value)
+{
+	return operator+=(FColor(Value,Value,Value,Value));
+}
+
+FColor FColor::operator-(uint16 Value) const
+{
+	return operator-(FColor(Value,Value,Value,Value));
+}
+
+FColor& FColor::operator-=(uint16 Value)
+{
+	return operator-=(FColor(Value,Value,Value,Value));
+}
+
+FColor FColor::operator*(float Value) const
+{
+	NColorPrivate::CheckMultiply(*this, Value);
+
+	FColor newColor;
+	newColor.R = R * Value;
+	newColor.G = G * Value;
+	newColor.B = B * Value;
+	newColor.A = A * Value;
+	return newColor;
+}
+
+FColor& FColor::operator*=(float Value)
+{
+	NColorPrivate::CheckMultiply(*this, Value);
+
+	R *= Value;
+	G *= Value;
+	B *= Value;
+	A *= Value;
+	return *this;
+}
+
+FColor FColor::operator/(float Value) const
+{
+	NColorPrivate::CheckDivide(*this, Value);
+
+	FColor newColor;
+	newColor.R = R / Value;
+	newColor.G = G / Value;
+	newColor.B = B / Value;
+	newColor.A = A / Value;
+	return newColor;
+}
+
+FColor& FColor::operator/=(float Value)
+{
+	NColorPrivate::CheckDivide(*this, Value);
+
+	R /= Value;
+	G /= Value;
+	B /= Value;
+	A /= Value;
+	return *this;
 }
